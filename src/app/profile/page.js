17a08002b596aac253acf5ac65d0d478483985f6
Belove <beloveolusola@gmail.com>
2024-11-06 23:58:21 -0700

    Profile Update

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,15 +2,25 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { redirect } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ProfilePage() {
     const session = useSession();
-    const [userName, setUserName] = useState(session?.data?.user?.name || '')
+    const [userName, setUserName] = useState('')
+    const [saved, setSaved] = useState(false)
+    const [isSaving, setIsSaving] = useState(false)
     const {status} = session;
+
+    useEffect(() => {
+        if (status === 'authenticated') {
+            setUserName(session.data.user.name);
+        }
+    }, [session, status]);
 
     async function handleProfileInfoUpdate(ev) {
         ev.preventDefault();
+        setSaved(false);
+        setIsSaving(true);
         const response = await fetch('/api/profile', {
             method: 'PUT',
             headers: {
@@ -19,7 +29,25 @@ export default function ProfilePage() {
             body: JSON.stringify({
                 name: userName,
             }),
-        })
+        });
+        setIsSaving(false);
+        if (response.ok) {
+            setSaved(true);
+        }
+    }
+    async function handleFileChange(ev) {
+        const files = ev.target.files;
+        if (files?.length === 1 && files) {
+            const data = new FormData;
+            data.set('files', files[0]);
+            await fetch('/api/upload', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+                body: data,
+            })
+        }
     }
 
     if (status === 'loading') {
@@ -38,11 +66,24 @@ export default function ProfilePage() {
                 Profile
             </h1>
             <div className="max-w-md mx-auto">
+                {saved && (
+                    <h2 className="text-center bg-green-100 p-4 rounded-lg border border-green-300">
+                        Profile saved!
+                    </h2>
+                )}
+                {isSaving && (
+                    <h2 className="text-center bg-red-100 p-4 rounded-lg border border-red-300">
+                        Saving profile...
+                    </h2>
+                )}
                 <div className="flex gap-4 items-center">
                     <div>
-                        <div className="p-2 rounded-lg">
-                            <Image className="rounded-lg h-full w-full mb-1" src={userImage} width={250} height={250} alt="avatar" />
-                            <button type="button">Edit</button>
+                        <div className="p-2 rounded-lg relative">
+                            <Image className="rounded-lg mb-1" src={userImage} width={100} height={100} alt="avatar" />
+                            <label>
+                                <input type="file" className="hidden" onChange={handleFileChange}/>
+                                <span className="border block border-gray-300 cursor-pointer text-center rounded-lg p-2">Edit</span>
+                            </label>
                         </div>
                     </div>
                     <form className="grow" onSubmit={handleProfileInfoUpdate}>
